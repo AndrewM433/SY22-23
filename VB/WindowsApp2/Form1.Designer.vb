1af3f881e8086a8002073ea64ce334ab61e60948
Andrew M <agmen433@learner.dvusd.org>
2022-08-17 08:26:19 -0700

    wednesday working version

---
+++ VB/WindowsApp2/Form1.Designer.vb
@@ -0,0 +1,281 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.conorbutton = New System.Windows.Forms.Button()
+        Me.displayPictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.conorpicturebox = New System.Windows.Forms.PictureBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.namelabel = New System.Windows.Forms.Label()
+        Me.Heightlabel = New System.Windows.Forms.Label()
+        Me.reachlabel = New System.Windows.Forms.Label()
+        Me.agelabel = New System.Windows.Forms.Label()
+        Me.tonypicturebox = New System.Windows.Forms.PictureBox()
+        Me.Tonybutton = New System.Windows.Forms.Button()
+        Me.khabibbox = New System.Windows.Forms.PictureBox()
+        Me.khabibbutton = New System.Windows.Forms.Button()
+        Me.dustinbox = New System.Windows.Forms.PictureBox()
+        Me.dustinbutton = New System.Windows.Forms.Button()
+        Me.charlesbox = New System.Windows.Forms.PictureBox()
+        Me.charlesbutton = New System.Windows.Forms.Button()
+        CType(Me.displayPictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.conorpicturebox, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.tonypicturebox, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.khabibbox, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.dustinbox, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.charlesbox, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'conorbutton
+        '
+        Me.conorbutton.Location = New System.Drawing.Point(12, 268)
+        Me.conorbutton.Name = "conorbutton"
+        Me.conorbutton.Size = New System.Drawing.Size(75, 23)
+        Me.conorbutton.TabIndex = 0
+        Me.conorbutton.Text = "Conor"
+        Me.conorbutton.UseVisualStyleBackColor = True
+        '
+        'displayPictureBox1
+        '
+        Me.displayPictureBox1.Location = New System.Drawing.Point(0, -2)
+        Me.displayPictureBox1.Name = "displayPictureBox1"
+        Me.displayPictureBox1.Size = New System.Drawing.Size(352, 225)
+        Me.displayPictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.displayPictureBox1.TabIndex = 1
+        Me.displayPictureBox1.TabStop = False
+        '
+        'conorpicturebox
+        '
+        Me.conorpicturebox.Image = CType(resources.GetObject("conorpicturebox.Image"), System.Drawing.Image)
+        Me.conorpicturebox.Location = New System.Drawing.Point(12, 338)
+        Me.conorpicturebox.Name = "conorpicturebox"
+        Me.conorpicturebox.Size = New System.Drawing.Size(109, 61)
+        Me.conorpicturebox.TabIndex = 2
+        Me.conorpicturebox.TabStop = False
+        Me.conorpicturebox.Visible = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(380, 13)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(44, 16)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Name"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(380, 57)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(46, 16)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Height"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(380, 111)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(47, 16)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "Reach"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(380, 166)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(31, 16)
+        Me.Label4.TabIndex = 6
+        Me.Label4.Text = "age"
+        '
+        'namelabel
+        '
+        Me.namelabel.AutoSize = True
+        Me.namelabel.Location = New System.Drawing.Point(447, 23)
+        Me.namelabel.Name = "namelabel"
+        Me.namelabel.Size = New System.Drawing.Size(0, 16)
+        Me.namelabel.TabIndex = 7
+        '
+        'Heightlabel
+        '
+        Me.Heightlabel.AutoSize = True
+        Me.Heightlabel.Location = New System.Drawing.Point(447, 57)
+        Me.Heightlabel.Name = "Heightlabel"
+        Me.Heightlabel.Size = New System.Drawing.Size(0, 16)
+        Me.Heightlabel.TabIndex = 8
+        '
+        'reachlabel
+        '
+        Me.reachlabel.AutoSize = True
+        Me.reachlabel.Location = New System.Drawing.Point(447, 111)
+        Me.reachlabel.Name = "reachlabel"
+        Me.reachlabel.Size = New System.Drawing.Size(0, 16)
+        Me.reachlabel.TabIndex = 9
+        '
+        'agelabel
+        '
+        Me.agelabel.AutoSize = True
+        Me.agelabel.Location = New System.Drawing.Point(447, 166)
+        Me.agelabel.Name = "agelabel"
+        Me.agelabel.Size = New System.Drawing.Size(0, 16)
+        Me.agelabel.TabIndex = 10
+        '
+        'tonypicturebox
+        '
+        Me.tonypicturebox.Image = CType(resources.GetObject("tonypicturebox.Image"), System.Drawing.Image)
+        Me.tonypicturebox.Location = New System.Drawing.Point(165, 338)
+        Me.tonypicturebox.Name = "tonypicturebox"
+        Me.tonypicturebox.Size = New System.Drawing.Size(109, 61)
+        Me.tonypicturebox.TabIndex = 12
+        Me.tonypicturebox.TabStop = False
+        Me.tonypicturebox.Visible = False
+        '
+        'Tonybutton
+        '
+        Me.Tonybutton.Location = New System.Drawing.Point(115, 268)
+        Me.Tonybutton.Name = "Tonybutton"
+        Me.Tonybutton.Size = New System.Drawing.Size(75, 23)
+        Me.Tonybutton.TabIndex = 11
+        Me.Tonybutton.Text = "Tony"
+        Me.Tonybutton.UseVisualStyleBackColor = True
+        '
+        'khabibbox
+        '
+        Me.khabibbox.Image = CType(resources.GetObject("khabibbox.Image"), System.Drawing.Image)
+        Me.khabibbox.Location = New System.Drawing.Point(302, 338)
+        Me.khabibbox.Name = "khabibbox"
+        Me.khabibbox.Size = New System.Drawing.Size(109, 61)
+        Me.khabibbox.TabIndex = 14
+        Me.khabibbox.TabStop = False
+        Me.khabibbox.Visible = False
+        '
+        'khabibbutton
+        '
+        Me.khabibbutton.Location = New System.Drawing.Point(208, 268)
+        Me.khabibbutton.Name = "khabibbutton"
+        Me.khabibbutton.Size = New System.Drawing.Size(66, 23)
+        Me.khabibbutton.TabIndex = 13
+        Me.khabibbutton.Text = "Khabib"
+        Me.khabibbutton.UseVisualStyleBackColor = True
+        '
+        'dustinbox
+        '
+        Me.dustinbox.Image = CType(resources.GetObject("dustinbox.Image"), System.Drawing.Image)
+        Me.dustinbox.Location = New System.Drawing.Point(437, 338)
+        Me.dustinbox.Name = "dustinbox"
+        Me.dustinbox.Size = New System.Drawing.Size(109, 61)
+        Me.dustinbox.TabIndex = 16
+        Me.dustinbox.TabStop = False
+        Me.dustinbox.Visible = False
+        '
+        'dustinbutton
+        '
+        Me.dustinbutton.Location = New System.Drawing.Point(291, 268)
+        Me.dustinbutton.Name = "dustinbutton"
+        Me.dustinbutton.Size = New System.Drawing.Size(75, 23)
+        Me.dustinbutton.TabIndex = 15
+        Me.dustinbutton.Text = "dustin"
+        Me.dustinbutton.UseVisualStyleBackColor = True
+        '
+        'charlesbox
+        '
+        Me.charlesbox.Image = CType(resources.GetObject("charlesbox.Image"), System.Drawing.Image)
+        Me.charlesbox.Location = New System.Drawing.Point(570, 338)
+        Me.charlesbox.Name = "charlesbox"
+        Me.charlesbox.Size = New System.Drawing.Size(109, 61)
+        Me.charlesbox.TabIndex = 18
+        Me.charlesbox.TabStop = False
+        Me.charlesbox.Visible = False
+        '
+        'charlesbutton
+        '
+        Me.charlesbutton.Location = New System.Drawing.Point(383, 268)
+        Me.charlesbutton.Name = "charlesbutton"
+        Me.charlesbutton.Size = New System.Drawing.Size(75, 23)
+        Me.charlesbutton.TabIndex = 17
+        Me.charlesbutton.Text = "charles"
+        Me.charlesbutton.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.charlesbox)
+        Me.Controls.Add(Me.charlesbutton)
+        Me.Controls.Add(Me.dustinbox)
+        Me.Controls.Add(Me.dustinbutton)
+        Me.Controls.Add(Me.khabibbox)
+        Me.Controls.Add(Me.khabibbutton)
+        Me.Controls.Add(Me.tonypicturebox)
+        Me.Controls.Add(Me.Tonybutton)
+        Me.Controls.Add(Me.agelabel)
+        Me.Controls.Add(Me.reachlabel)
+        Me.Controls.Add(Me.Heightlabel)
+        Me.Controls.Add(Me.namelabel)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.conorpicturebox)
+        Me.Controls.Add(Me.displayPictureBox1)
+        Me.Controls.Add(Me.conorbutton)
+        Me.Name = "Form1"
+        Me.Text = " "
+        CType(Me.displayPictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.conorpicturebox, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.tonypicturebox, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.khabibbox, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.dustinbox, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.charlesbox, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents conorbutton As Button
+    Friend WithEvents displayPictureBox1 As PictureBox
+    Friend WithEvents conorpicturebox As PictureBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents namelabel As Label
+    Friend WithEvents Heightlabel As Label
+    Friend WithEvents reachlabel As Label
+    Friend WithEvents agelabel As Label
+    Friend WithEvents tonypicturebox As PictureBox
+    Friend WithEvents Tonybutton As Button
+    Friend WithEvents khabibbox As PictureBox
+    Friend WithEvents khabibbutton As Button
+    Friend WithEvents dustinbox As PictureBox
+    Friend WithEvents dustinbutton As Button
+    Friend WithEvents charlesbox As PictureBox
+    Friend WithEvents charlesbutton As Button
+End Class
---
+++ VB/WindowsApp2/Form1.vb
@@ -0,0 +1,47 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles conorbutton.Click
+        displayPictureBox1.Image = conorpicturebox.Image
+        namelabel.Text = "Conor Mcgregor"
+        Heightlabel.Text = "5Ft,9"
+        reachlabel.Text = "74inch"
+        agelabel.Text = "34"
+    End Sub
+
+    Private Sub Heightlabel_Click(sender As Object, e As EventArgs) Handles Heightlabel.Click
+
+    End Sub
+
+    Private Sub Tonybutton_Click(sender As Object, e As EventArgs) Handles Tonybutton.Click
+        displayPictureBox1.Image = tonypicturebox.Image
+        namelabel.Text = "Tony Ferguson"
+        Heightlabel.Text = "5Ft,11"
+        reachlabel.Text = "76inch"
+        agelabel.Text = "38"
+    End Sub
+
+    Private Sub khabibbutton_Click(sender As Object, e As EventArgs) Handles khabibbutton.Click
+        displayPictureBox1.Image = khabibbox.Image
+        namelabel.Text = "Khabib Nurmagomedov"
+        Heightlabel.Text = "5Ft,10"
+        reachlabel.Text = "73inch"
+        agelabel.Text = "33"
+    End Sub
+
+
+
+    Private Sub dustinbutton_Click(sender As Object, e As EventArgs) Handles dustinbutton.Click
+        displayPictureBox1.Image = dustinbox.Image
+        namelabel.Text = "Dustin Poirier"
+        Heightlabel.Text = "5Ft,9"
+        reachlabel.Text = "74inch"
+        agelabel.Text = "33"
+    End Sub
+
+    Private Sub charlesbutton_Click(sender As Object, e As EventArgs) Handles charlesbutton.Click
+        displayPictureBox1.Image = charlesbox.Image
+        namelabel.Text = "Charles Oliveira"
+        Heightlabel.Text = "5Ft,11"
+        reachlabel.Text = "76inch"
+        agelabel.Text = "32"
+    End Sub
+End Class
